132fa8b34d5b829457ffee3c9aade7dcc77f6561
Levi Miller <paullevimiller@gmail.com>
2019-09-12 09:59:47 -0700

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+const random = Math.random()
+
+const mod = 24 % 5;
+
+const max = Math.max(20, 1, 6, 4, 12);
